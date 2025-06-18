94903f43d7bf81021b16e9fde410e9d0a0c95a25
muthanii <muthanalsaadi@gmail.com>
2025-06-18 21:13:53 +0300

    feat: add hello world.

---
+++ solutions.cs
@@ -0,0 +1,11 @@
+// Hello world
+using System;
+
+class Solutions
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+// This is a simple C# program that prints "Hello, World!" to the console.
